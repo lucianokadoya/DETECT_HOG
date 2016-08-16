45367ff78310fb199aa14758e1574b0014609b62
Luciano Kadoya <luciano.kadoya@gmail.com>
2016-08-16 07:02:14 -0700

    Improvements

--- detect_HOG.py
+++ detect_HOG.py
@@ -4,7 +4,13 @@
 import numpy as np
 from imutils.object_detection import non_max_suppression
 import cv2
+
 passTotal = 0
+file = raw_input("Entre com o arquivo: ")
+reg1 = input("Entre com a altura inicial de deteccao: ")
+reg2 = input("Entre com a altura final de deteccao: ")
+reg3 = input("Entre com a largura inicial de deteccao: ")
+reg4 = input("Entre com a largura final de deteccao: ")
 
 def draw_detections(img, rects, thickness = 1):
     global passTotal
@@ -23,10 +29,10 @@ if __name__ == '__main__':
 
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
-    cap=cv2.VideoCapture('video2.mp4')
+    cap=cv2.VideoCapture(str(file))
     while True:
         _,frame=cap.read()
-        frame2 = frame[100:150, 0:850]
+        frame2 = frame[reg1:reg2, reg3:reg4]
         found,w=hog.detectMultiScale(frame2, winStride=(8,8), padding=(32,32), scale=1.05)
         draw_detections(frame2,found)
         cv2.imshow('feed',frame2)
